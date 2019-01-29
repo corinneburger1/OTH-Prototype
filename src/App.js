7de9139b9e62c4a8526eed42b838ac27393b1c35
Corinne Burger <corinneburger1@gmail.com>
2019-01-29 10:53:03 -0600

    Static Prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class SubscriptionInfo extends Component {
+  render() {
+    var onclick = () => window.open(this.props.subscription.unsubscribeLink,'_blank');
+    var daysRemainingClass = this.props.subscription.daysToPayment <= 7 ? "subscription_box_days_expiring" : "subscription_box_days";
+    return (
+      <div>
+        <div class={daysRemainingClass}>Days remaining: {this.props.subscription.daysToPayment}</div>
+        <div class="subscription_box_price">Renewal cost: ${this.props.subscription.cost}</div>
+        <button class="Button" onClick={onclick}>Unsubscribe</button>
+      </div>
+    );
+  }
+}
+
+class SubscriptionItem extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="subscription_box">
+        <div class="subscription_box_title">{this.props.subscription.name}</div>
+        <SubscriptionInfo subscription={this.props.subscription}/>
+        <img class="subsciption_image" src={require(`./static/hulu.jpg`)}/>
+      </div>
+    );
+  }
+}
+
+class SubscriptionTable extends Component {
+  render() {
+    var subscription_list = [];
+    if(this.props.subscriptions) {
+      this.props.subscriptions.forEach(function(subscription) {
+        subscription_list.push(
+          <SubscriptionItem subscription={subscription}/>
+        );
+      });
+    }
+    return (
+      <ul>{subscription_list}</ul>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) 
+  {
+    super(props);
+    this.state = {
+      subscriptions: null,
+    }
+  }
+
+  componentDidMount() {
+    import("./subscriptions.json")
+    .then(json => this.setState({subscriptions: json.default.subscriptions}))
+  }
+
+  render() {
+    console.log(this.state.subscriptions);
+    return (
+      <div>
+        <div style={{fontSize: 60 + 'px'}}>Manage Subscriptions</div>
+        <SubscriptionTable subscriptions={this.state.subscriptions}/>
       </div>
     );
   }
