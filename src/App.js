9e396e5c266adaea3bbe50dbaa59eb81a4821fb6
Corinne Burger <corinneburger1@gmail.com>
2019-01-29 12:26:59 -0600

    closes #5

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class SubscriptionInfo extends Component {
   render() {
-    var onclick = () => window.open(this.props.subscription.unsubscribeLink,'_blank');
+    var onclick = () => window.open(this.props.subscription.cancelLink,'_blank');
     var daysRemainingClass = this.props.subscription.daysToPayment <= 7 ? "subscription_box_days_expiring" : "subscription_box_days";
     return (
       <div>
         <div class={daysRemainingClass}>Days remaining: {this.props.subscription.daysToPayment}</div>
-        <div class="subscription_box_price">Renewal cost: ${this.props.subscription.cost}</div>
+        <div class="subscription_box_price">${this.props.subscription.cost} {this.props.subscription.subscriptionType}</div>
         <button class="Button" onClick={onclick}>Unsubscribe</button>
       </div>
     );
@@ -40,7 +40,31 @@ class SubscriptionTable extends Component {
       });
     }
     return (
-      <ul>{subscription_list}</ul>
+      <div class="subscription_table">
+        {subscription_list}
+      </div>
+    );
+  }
+}
+
+class SubscriptionSummary extends Component {
+  render() {
+    var anualCost = 0;
+    var subscriptionCount = 0;
+    if(this.props.subscriptions) {
+      this.props.subscriptions.forEach(function(subscription) {
+        if(subscription.subscriptionType == "monthly"){
+          anualCost += 12 * subscription.cost;
+        } else if(subscription.subscriptionType == "yearly"){
+          anualCost += subscription.cost;
+        } else if(subscription.subscriptionType == "twice yearly"){
+          anualCost += 2 * subscription.cost;
+        }
+        subscriptionCount += 1;
+      });
+    }
+    return (
+      <div class="summary">You are spending <anualcost>${anualCost}</anualcost> each year on {subscriptionCount} subscriptions.</div>
     );
   }
 }
@@ -65,6 +89,7 @@ class App extends Component {
       <div>
         <div class="page_title">Manage Subscriptions</div>
         <SubscriptionTable subscriptions={this.state.subscriptions}/>
+        <SubscriptionSummary subscriptions={this.state.subscriptions}/>
       </div>
     );
   }
